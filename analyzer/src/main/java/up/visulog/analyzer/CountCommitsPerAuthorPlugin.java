e23c732ecd9d175b231787cbe0bf0f98bc2d7c93
KAABECHE rayane <rayanoo1302@gmail.com>
2020-09-29 11:53:30 +0200

    Update CountCommitsPerAuthorPlugin.java

--- analyzer/src/main/java/up/visulog/analyzer/CountCommitsPerAuthorPlugin.java
+++ analyzer/src/main/java/up/visulog/analyzer/CountCommitsPerAuthorPlugin.java
@@ -7,6 +7,10 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import java.io.File;  
+import java.io.IOException;
+import java.io.FileWriter;
+
 public class CountCommitsPerAuthorPlugin implements AnalyzerPlugin {
     private final Configuration configuration;
     private Result result;
@@ -54,6 +58,21 @@ public class CountCommitsPerAuthorPlugin implements AnalyzerPlugin {
                 html.append("<li>").append(item.getKey()).append(": ").append(item.getValue()).append("</li>");
             }
             html.append("</ul></div>");
+            try {
+              File myObj = new File("index.html"); // create an html file
+              if (myObj.createNewFile()) {
+                System.out.println("HTML File created: " + myObj.getName());
+                System.out.println("The html file has been created in the client folder.");
+              } else {
+                System.out.println("File already exists.");
+              }
+              FileWriter myWriter = new FileWriter("index.html");// write inside the html file
+              myWriter.write(html.toString());
+              myWriter.close();
+            } catch (IOException e) {
+              System.out.println("An error occurred.");
+              e.printStackTrace();
+            }
             return html.toString();
         }
     }
