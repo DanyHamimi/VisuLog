bcc0945a36b5b4b03523b79f148f3d5c945b1440
HAMIMI dany <dany.hamimi@gmail.com>
2020-11-02 23:46:02 +0100

    Update CountCommitsPerAuthorPlugin.java

--- analyzer/src/main/java/up/visulog/analyzer/CountCommitsPerAuthorPlugin.java
+++ analyzer/src/main/java/up/visulog/analyzer/CountCommitsPerAuthorPlugin.java
@@ -53,11 +53,14 @@ public class CountCommitsPerAuthorPlugin implements AnalyzerPlugin {
 
         @Override
         public String getResultAsHtmlDiv() {
-            StringBuilder html = new StringBuilder("<div>Commits per author: <ul>");
+            StringBuilder html = new StringBuilder("<!DOCTYPE HTML><html><head>");
+            html.append("\"<script src=\"https://canvasjs.com/assets/script/canvasjs.min.js\"></script>\"");
+            html.append("<script type=\"text/javascript\">");
+            html.append("window.onload = function () { var chart = new CanvasJS.Chart(\"chartContainer\", {title:{ text: \"Commits per author:\"},data: [{type: \"pie\",dataPoints: [");
             for (var item : commitsPerAuthor.entrySet()) {
-                html.append("<li>").append(item.getKey()).append(": ").append(item.getValue()).append("</li>");
+                html.append("{ label: \"").append(item.getKey()).append("\" , y:").append(item.getValue()).append("  },");
             }
-            html.append("</ul></div>");
+            html.append("] } ] }); chart.render(); } </script> </head> <body> <div id=\"chartContainer\" style=\"height: 600px; width: 100%;\"></div> </body> </html>");
             try {
               File myObj = new File("index.html"); // create an html file
               if (myObj.createNewFile()) {
