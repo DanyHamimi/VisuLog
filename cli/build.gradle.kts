48450543a54242ff7d40e33a65330a69d31bfe53
AlazawiRayan <ryanalaz11@gmail.com>
2020-11-16 12:18:41 +0100

    forgot to commit cli/build.gradle.kts

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -3,10 +3,13 @@ plugins {
     `java-library`
 }
 
+application.mainClass.set("up.visulog.cli.CliApplication")
+
 dependencies {
     implementation(project(":config"))
     implementation(project(":gitrawdata"))
     testImplementation("junit:junit:4.+")
+	
 }
 
 
