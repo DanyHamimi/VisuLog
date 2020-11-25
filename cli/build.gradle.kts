c7c2835111bd6e5f62a63068b27e9f16586489da
FOURMONT baptiste <baptiste7512@gmail.com>
2020-11-25 10:10:35 +0100

    Update build.gradle.kts Add Analyser useful for add args to run config

--- cli/build.gradle.kts
+++ cli/build.gradle.kts
@@ -1,4 +1,3 @@
-
 plugins {
 	java
 	application
@@ -7,6 +6,7 @@ plugins {
 application.mainClass.set("up.visulog.cli.CliApplication")
 
 dependencies {
+    implementation(project(":analyzer"))
     implementation(project(":config"))
     implementation("org.slf4j:slf4j-nop:1.7.30")
     implementation(project(":gitrawdata"))
